b49ed06523927ea9efe6c6fb04aa0d732d9316af
Guilherme <guisil_ps@hotmail.com>
2020-03-25 10:57:03 -0300

    Refactoring LoginActivity.kt (showErrorMessage, validateFields)

--- app/src/main/java/com/example/treinamentotestes/LoginActivity.kt
+++ app/src/main/java/com/example/treinamentotestes/LoginActivity.kt
@@ -3,11 +3,10 @@ package com.example.treinamentotestes
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.TextView
+import androidx.annotation.StringRes
 import androidx.appcompat.app.AlertDialog
 import com.example.treinamentotestes.utils.PasswordValidator
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
@@ -15,38 +14,26 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val username: TextView = findViewById(R.id.username)
-        val password: TextView = findViewById(R.id.password)
-        val btnLogin: Button = findViewById(R.id.login)
-
-        btnLogin.setOnClickListener(){
+        login.setOnClickListener() {
             if (validateFields(username.text.toString(), password.text.toString())) {
                 startActivity(Intent(applicationContext, HomeActivity::class.java))
             }
         }
-
     }
 
-    private fun showErrorMessage(message: String) {
-        val alertDialogBuilder = AlertDialog.Builder(this)
-        val alertDialog: AlertDialog
-
-        alertDialogBuilder.setMessage(message)
-        alertDialog = alertDialogBuilder.create()
-        alertDialog.show()
+    private fun showErrorMessage(@StringRes errorMessage: Int) {
+        AlertDialog.Builder(this)
+            .setMessage(errorMessage)
+            .show()
     }
 
     private fun validateFields(username: String, password: String): Boolean {
-        val errorMessage: String
-
-        if (username.isEmpty())
-            errorMessage = getString(R.string.empty_username_error)
-        else if (password.isEmpty())
-            errorMessage = getString(R.string.empty_password_error)
-        else if (!PasswordValidator().isValid(password))
-            errorMessage = getString(R.string.invalid_password_error)
-        else
-            return true
+        val errorMessage = when {
+            username.isEmpty() -> R.string.empty_username_error
+            password.isEmpty() -> R.string.empty_password_error
+            !PasswordValidator().isValid(password) -> R.string.invalid_password_error
+            else -> return true
+        }
 
         showErrorMessage(errorMessage)
         return false
